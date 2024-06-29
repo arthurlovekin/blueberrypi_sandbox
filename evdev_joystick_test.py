adde9fbef5b3a68ea13c9812b79d7d6c7b2f3a09
Arthur Lovekin <arthurlovekin@gmail.com>
2024-06-28 20:38:40 -0500

    added prints for joystick buttons of interest

--- evdev_joystick_test.py
+++ evdev_joystick_test.py
@@ -21,11 +21,24 @@ print(device.capabilities(verbose=True))
 
 async def main(device):
     async for event in device.async_read_loop():
+        # types (event_factory): 0: SyncEvent; 1: KeyEvent; 2: RelEvent, 3: AbsEvent
         if event.type == evdev.ecodes.EV_KEY:
-            print(evdev.categorize(event)) # categorize gives a timestamp and the button type
+            # print(f"{event.type=} {event.timestamp()} {event.code=}~{evdev.resolve_ecodes(evdev.ecodes.BTN, [event.code])} {event.value=}")
+            print(f"{evdev.categorize(event)} : {event.value}") # categorize gives a timestamp and the button type
+            if event.code == evdev.ecodes.BTN_TL2:
+                print("Got BTN_TL2")
+            elif event.code == evdev.ecodes.BTN_TR2:
+                print("Got BTN_TR2")        
+        
         elif event.type == evdev.ecodes.EV_ABS:
+            if event.code == evdev.ecodes.ABS_X:
+                print("Got ABS_X")
+            elif event.code == evdev.ecodes.ABS_Y:
+                print("Got ABS_Y")
+            elif event.code == evdev.ecodes.ABS_RX:
+                print("Got ABS_RX")
             print(f"{evdev.categorize(event)} : {event.value}")
-        # print(repr(ev))
+        # print(repr(event))
 
 if __name__ == "__main__":
     asyncio.run(main(device))
