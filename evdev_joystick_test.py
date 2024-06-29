9be5dcba5b92473138b22f22ca271599173e230d
Arthur Lovekin <arthurlovekin@gmail.com>
2024-06-28 19:39:44 -0500

    evdev asyncio

--- evdev_joystick_test.py
+++ evdev_joystick_test.py
@@ -1,4 +1,6 @@
+import asyncio
 import evdev
+
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 print("--- Available devices ---")
 for device in devices:
@@ -10,9 +12,20 @@ print("---device capabilities---")
 
 print(device.capabilities(verbose=True))
 
+## if you want without asyncio
+# for event in device.read_loop():
+#     if event.type == evdev.ecodes.EV_KEY:
+#         print(evdev.categorize(event)) # categorize gives a timestamp and the button type
+#     elif event.type == evdev.ecodes.EV_ABS:
+#         print(f"{evdev.categorize(event)} : {event.value}")
 
-for event in device.read_loop():
-    if event.type == evdev.ecodes.EV_KEY:
-        print(evdev.categorize(event)) # categorize gives a timestamp and the button type
-    elif event.type == evdev.ecodes.EV_ABS:
-        print(f"{evdev.categorize(event)} : {event.value}")
+async def main(device):
+    async for event in device.async_read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            print(evdev.categorize(event)) # categorize gives a timestamp and the button type
+        elif event.type == evdev.ecodes.EV_ABS:
+            print(f"{evdev.categorize(event)} : {event.value}")
+        # print(repr(ev))
+
+if __name__ == "__main__":
+    asyncio.run(main(device))
